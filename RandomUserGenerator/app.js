446d434e0148b4d607c15699ac97b294b11e90e1
ergunDevv <ergncyln2127088@gmail.com>
2023-02-11 11:13:18 +0300

    Initial project for random user

---
+++ RandomUserGenerator/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    template : '<h1>Hello {{firstName}}</h1>',
+    data () {
+        return {
+            firstName: 'John'
+        }
+    }
+
+})
+
+app.mount('#app');
